b4a9aac96c171999529c281da04e618c78030eda
sgunti137 <67163309+sgunti137@users.noreply.github.com>
2021-06-15 13:31:30 +0530

    Update models.py
    update date and time

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ class Resume(models.Model):
     name = models.CharField(max_length = 40)
     pdfFile = models.CharField(max_length = 40)
     latexFile = models.CharField(max_length = 40)
+    date = models.CharField(max_length = 30)
 
     def __str__(self):
         return self.name
